3232f6e6c39dfb936089440dc0b148fe5d7c98e6
mothran <w.parker.thompson@gmail.com>
2012-01-18 09:57:35 -0800

    restructured the encryption, send/recv classes.  Working crypto and send/recv (durfix)

--- bunny.py
+++ bunny.py
@@ -38,9 +38,9 @@ import binascii
 iface = "wlan1"
 driver = "rtl8187"
 chan = 6
-modulus = 4.5
-remainder = 3.5
-	# for AES 256 the key has too be 32 bytes long.
+modulus = 3.6
+remainder = 1.2
+	# for AES 256 the key has to be 32 bytes long.
 AESkey = "B" * 32
 	# for code that sets new key:
 	# password = 'kitty'
@@ -53,9 +53,9 @@ class AEScrypt():
 	# http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/
 	
 	# How the IV is transmited: 
-	# [ - - - 16B - - - -][ - - - - NB - - - - - -]
+	# [ - - - 16B - - - -][ 2B ][ - - - - NB - - - - - -]
 	# first 16 bytes for the message is the IV. 
-
+	# bytes 17 and 18 are the number of blocks to read. (int short)
 
 	def __init__(self):
 		
@@ -70,74 +70,45 @@ class AEScrypt():
 		# returns a block of string of cipher text
 		iv = os.urandom(16)
 		encryptor = AES.new(AESkey, self.mode, iv)
-		encoded = "%s%s" % (iv, (data + (self.blockSize - len(data) % self.blockSize) * self.padding))
-		
-		# we might want to remove the base64 encoding for when it is actually on air.
-		return base64.b64encode(encryptor.encrypt(encoded))
+		encoded = "%s" % (data + (self.blockSize - len(data) % self.blockSize) * self.padding)
+		encoded = encryptor.encrypt(encoded)
+		block_count = len(encoded) / 32
+		block_count = struct.pack("H", block_count)
+		output = iv + block_count + encoded
+		return output
 		
 	def decrypt(self, data):
 		
-		# return a block of plaintext 
-		output = base64.b64decode(data)
-		iv = output[:16]
-		raw = output[16:]
+		# return a block of plaintext
+		iv = data[:16]
+		block_count = struct.unpack("H", data[16:18])
+		block_count = block_count[0] * 32 + 18
+		raw = data[18:block_count]
 		encryptor = AES.new(AESkey, self.mode, iv)
-		return encryptor.decrypt(raw).rstrip(self.padding)
-	
-# end of Crypto class
-
+		try:
+			Eoutput = encryptor.decrypt(raw).rstrip(self.padding)
+		except:
+			print "Bad Packet legnth, consider resending"
+			
+		return Eoutput
+		
 class SendRec():
 	
-	# These send/rec functions should be used in hidden / paranoid mode.
-	def sendPacket(self, data):
-		# send dat shit!
-		self.lorcon.txpacket(data)
-	def recPacket(self):
-		# return the raw packet if the mod/remain value is correct. 
-		run = True 
-		while(run):
-			header, rawPack = self.pcapy.next()
-			size = len(rawPack)
-			if (size % modulus == remainder):
-				run = False
-					# fmt:
-					#	H = unsigned short
-				size = struct.unpack("<H", rawPack[2:4])
-				size = int(size[0])
-				rawPack = rawPack[size:]
-				return rawPack
-	
-	# these functions should be used if you dont care about being noticed
-	def sendPacketDurFix(self, data):
-		data = "\x00\x00\x00\x00" + data
-		self.lorcon.txpacket(data)
-	
-	def recPacketDurFix(self):
-		# return the raw packet if the mod/remain value is correct. 
-		run = True 
-		while(run):
-			header, rawPack = self.pcapy.next()
-			size = len(rawPack)
-			if (size % modulus == remainder):
-				run = False
-					# fmt:
-					#	H = unsigned short
-				sizeHead = struct.unpack("<H", rawPack[2:4])
-				sizeHead = int(sizeHead[0]) + 4
-				rawPack = rawPack[sizeHead:]
-				return rawPack
-	
-		
-	def start(self):
+	def __init__(self):
 		# initilize all that shit. 
-		# might consider moving this to a def: __init__
 		
 		try:
 			self.lorcon = pylorcon.Lorcon(iface, driver)
 		except:
 			print "Error creating lorcon object, try running as root"
 			exit()
-		self.lorcon.setmode("MONITOR");
+		
+		# check for monitor mode, if not already in monitor mode, make it.
+		if (self.lorcon.getmode() != "MONITOR"):
+			os.system("ifconfig " + iface + " down");
+			self.lorcon.setmode("MONITOR");
+			os.system("ifconfig " + iface + " up");
+		
 		self.lorcon.setfunctionalmode("INJECT");
 		self.lorcon.setchannel(chan);
 		
@@ -150,6 +121,47 @@ class SendRec():
 			self.pcapy = open_live(iface, MAX_LEN, PROMISCUOUS, READ_TIMEOUT)
 		except:
 			print "Error creating pcapy descriptor, try turning on the target interface or setting it to monitor mode"
+	
+	
+	# These send/rec functions should be used in hidden / paranoid mode.
+	def sendPacket(self, data):
+		self.lorcon.txpacket(data)
+	def recPacket(self):
+		# return the raw packet if the mod/remain value is correct. 
+		run = True 
+		while(run):
+			header, rawPack = self.pcapy.next()
+			size = len(rawPack)
+			if (size % modulus == remainder):
+				run = False
+				# H = unsigned short
+				size = struct.unpack("<H", rawPack[2:4])
+				size = int(size[0])
+				rawPack = rawPack[size:]
+				return rawPack
+	
+	# these functions should be used if you dont care about being noticed
+	def sendPacketDurFix(self, data):
+		data = "\x00\x00\x00\x00" + data
+		
+		# Create a packet with proper length, add radiotap header length + zeros.
+		while( round((len(data) + 13) % modulus, 2) != remainder):
+			data = data + os.urandom(1);
+		self.lorcon.txpacket(data)
+	
+	def recPacketDurFix(self):
+		# return the raw packet if the mod/remain value is correct. 
+		run = True 
+		while(run):
+			header, rawPack = self.pcapy.next()
+			size = len(rawPack)
+			if (round(size % modulus, 2) == remainder):
+				run = False
+				# H = unsigned short
+				sizeHead = struct.unpack("<H", rawPack[2:4])
+				sizeHead = int(sizeHead[0]) + 4
+				rawPack = rawPack[sizeHead:]
+				return rawPack
 	
 	def reloop(self):
 		# This exists only for testing purposes.
@@ -167,16 +179,22 @@ class SendRec():
 		packPerSec = packNum / totalTime
 		print "Total Packets (p/s): %s" % packPerSec
 
-# end of sendRec Class
 
-#crypter = AEScrypt()
-#output = crypter.encrypt("Hello world")
-#print "chiphertext: %s" % output
-#result = crypter.decrypt(output)
-#rint "plaintext:   %s" % result
-
+crypter = AEScrypt()
+output = crypter.encrypt("HELLO WORLD" * 30)
+print "chiphertext: %s" % binascii.hexlify(output)
+print "chiphertext length: %s" % len(output);
 sandr = SendRec()
-sandr.start()
-sandr.sendPacketDurFix("HELLO LOVELY WOMAN")
+sandr.sendPacketDurFix(output)
 input = sandr.recPacketDurFix()
-print input
+result = crypter.decrypt(input)
+print "plaintext:   %s" % result
+
+#sandr = SendRec()
+#sandr.sendPacketDurFix("HELLO LOVELY WOMAN")
+#input = sandr.recPacketDurFix()
+#print input
+
+#object = SendRec()
+#object.start()
+#object.reloop()
